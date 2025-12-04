5cf8d90342f602a6b0f6b1c3dcfc26bc54a18c58
kovacsnandor <kovacsnandor@gmail.com>
2025-12-04 13:19:04 +0100

    PlayingsportTest providerrel is

--- server/tests/Unit/PlayingsportTest.php
+++ server/tests/Unit/PlayingsportTest.php
@@ -4,14 +4,47 @@ namespace Tests\Unit;
 
 use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 class PlayingsportTest extends TestCase
 {
     protected $expectedSchema = [
-        'id'         => 'bigint',
-        'studentId'       => 'bigint',
-        'sportId'       => 'bigint',
+        'id' => 'bigint',
+        'studentId' => 'bigint',
+        'sportId' => 'bigint',
     ];
+
+    public static function expectedSchemaDataProvider()
+    {
+        return [
+            'id oszlop' => ['id', 'bigint'],
+            'studentId oszlop' => ['studentId', 'bigint'],
+            'sportId oszlop' => ['sportId', 'bigint'],
+        ];
+    }
+
+    #[DataProvider('expectedSchemaDataProvider')]
+    public function test_does_the_playingsports_table_contain_all_fields_provider(string $column, string $type): void
+    {
+        //Ellenőrizze, hogy megvannak-e a tábla mezői
+        $this->assertTrue(Schema::hasColumn('playingsports', $column), "A '$column' oszlop nem letezik");
+    }
+
+    #[DataProvider('expectedSchemaDataProvider')]
+    public function test_the_playingsports_table_columns_have_the_expected_types_provider($columnName, $expectedLaravelType)
+    {
+        //Ellenőrizze, hogy jók-e a típusai
+
+        $actualDbSqlType = Schema::getColumnType('playingsports', $columnName);
+
+        $isTypeMatch = $actualDbSqlType == $expectedLaravelType;
+        $this->assertTrue(
+            $isTypeMatch,
+            "A '{$columnName}' oszlop típusa nem egyezik. Várt: '{$expectedLaravelType}', Kapott DB-típus: '{$actualDbSqlType}'."
+        );
+    }
+
+
 
     public function test_exists_playingsports_table(): void
     {
@@ -22,9 +55,9 @@ class PlayingsportTest extends TestCase
 
     public function test_does_the_playingsports_table_contain_all_fields(): void
     {
-       //Ellenőrizze, hogy megvannak-e a tábla mezői
+        //Ellenőrizze, hogy megvannak-e a tábla mezői
         foreach ($this->expectedSchema as $column => $type) {
-            
+
             $this->assertTrue(Schema::hasColumn('playingsports', $column), "A '$column' oszlop nem letezik");
         }
     }
@@ -35,16 +68,16 @@ class PlayingsportTest extends TestCase
 
         $columns = Schema::getColumnListing('playingsports');
 
- 
+
         $this->assertEmpty(
             array_diff(array_keys($this->expectedSchema), $columns),
             'Hiányzó oszlopok a students táblában.'
         );
- 
+
         foreach ($this->expectedSchema as $columnName => $expectedLaravelType) {
- 
+
             $actualDbSqlType = Schema::getColumnType('playingsports', $columnName);
-           
+
             $isTypeMatch = $actualDbSqlType == $expectedLaravelType;
             $this->assertTrue(
                 $isTypeMatch,
